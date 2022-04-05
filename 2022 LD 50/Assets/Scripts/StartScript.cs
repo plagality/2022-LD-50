33b78985a17b2515076cd39d798683a1e08f9011
plagality <93016934+plagality@users.noreply.github.com>
2022-04-04 17:10:58 -0700

    Title Update

--- 2022 LD 50/Assets/Scripts/StartScript.cs
+++ 2022 LD 50/Assets/Scripts/StartScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScript : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class StartScript : MonoBehaviour
     void Update()
     {
         if (Input.GetButtonDown("Submit")) {
-            //LoadSceneAsync
+            SceneManager.LoadScene("Main");
         }
     }
 }
